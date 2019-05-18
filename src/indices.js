6861fa2380f4eef90d5eda7fd7955647491eef41
tony <anthony.kerz@gmail.com>
2019-05-18 03:26:41 -0400

    bumps

--- src/indices.js
+++ src/indices.js
@@ -5,26 +5,26 @@ import _ from 'lodash'
 
 // const dbg = debug('app:helpr:indices')
 
-// eslint-disable-next-line import/prefer-default-export
 export function findDeepIndices({array, path, predicate}) {
-  // dbg('array=%o, path=%o', stringify(array), path)
-  assert(_.isArray(array), 'array required')
-  assert(_.isArray(path), 'path array required')
-  if (path.length > 0) {
-    const result = _.transform(array, (result, val, index) => {
-      // dbg('result=%o, val=%o, index=%o', result, stringify(val), index)
-      const childArray = _.get(val, path[0])
-      if (_.isArray(childArray)) {
-        const childIndices = findDeepIndices({array: childArray, path: path.slice(1), predicate})
-        if (childIndices) {
-          result.push(index)
-          result.push(...childIndices)
-          return false
-        }
-      }
-    })
-    return _.isEmpty(result) ? null : result
-  }
-  const index = _.findIndex(array, predicate)
-  return index < 0 ? null : [index]
+	// dbg('array=%o, path=%o', stringify(array), path)
+	assert(_.isArray(array), 'array required')
+	assert(_.isArray(path), 'path array required')
+	if (path.length > 0) {
+		const result = _.transform(array, (result, val, index) => {
+			// dbg('result=%o, val=%o, index=%o', result, stringify(val), index)
+			const childArray = _.get(val, path[0])
+			if (_.isArray(childArray)) {
+				const childIndices = findDeepIndices({array: childArray, path: path.slice(1), predicate})
+				if (childIndices) {
+					result.push(index)
+					result.push(...childIndices)
+					return false
+				}
+			}
+		})
+		return _.isEmpty(result) ? null : result
+	}
+
+	const index = _.findIndex(array, predicate)
+	return index < 0 ? null : [index]
 }
